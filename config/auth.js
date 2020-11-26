05f4d2564c5a944cc09b31bdf43b61498039544f
Jonel T. Lenteria <JLenteria@RDPIHOCEBU.COM>
2020-11-26 13:38:48 +0800

    commit

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/user");
 const keys = require("./keys");
 
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
@@ -15,33 +15,33 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-      proxy: true,
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(profile);
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({
-            //googleId: profile.id,
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            photo: profile.photos[0].value.split("?")[0],
-          })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
-    }
-  )
-);
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: keys.googleClientID,
+//       clientSecret: keys.googleClientSecret,
+//       callbackURL: "/auth/google/callback",
+//       proxy: true,
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       console.log(profile);
+//       User.findOne({ googleId: profile.id }).then((existingUser) => {
+//         if (existingUser) {
+//           done(null, existingUser);
+//         } else {
+//           new User({
+//             //googleId: profile.id,
+//             name: profile.displayName,
+//             email: profile.emails[0].value,
+//             photo: profile.photos[0].value.split("?")[0],
+//           })
+//             .save()
+//             .then((user) => done(null, user));
+//         }
+//       });
+//     }
+//   )
+// );
 
 const auth = async (req, res, next) => {
   const token = req.header("Authorization").replace("Bearer ", "");
